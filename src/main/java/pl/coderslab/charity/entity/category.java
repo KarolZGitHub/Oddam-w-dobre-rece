2c198d62bfcbb24412764d7a2dd32342350db661
Karol <karol.zimolag@wp.pl>
2023-07-16 11:25:30 +0200

    Added category entity and footer fix.

---
+++ src/main/java/pl/coderslab/charity/entity/category.java
@@ -0,0 +1,17 @@
+package pl.coderslab.charity.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "category")
+@Data
+public class category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @NotBlank
+    private String name;
+}
